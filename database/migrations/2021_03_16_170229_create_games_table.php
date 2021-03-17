5030b82ee3071fa879705dc505579e6f093b4b07
cochimu <cochimu1221@icloud.com>
2021-03-17 20:05:10 +0900

    css,js

---
+++ database/migrations/2021_03_16_170229_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('name');
+            $table->text('describe');
+            $table->integer('play_time');
+            $table->integer('players_minimum');
+            $table->integer('players_max');
+            $table->string('image_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
